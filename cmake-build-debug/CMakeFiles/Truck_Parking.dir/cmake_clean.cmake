fcf177e09fd95a6244e6e8457aacb47873529a56
bravoila <mcl1001622@gmail.com>
2018-03-25 18:03:52 -0500

    Mar 25 2018

---
+++ cmake-build-debug/CMakeFiles/Truck_Parking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Truck_Parking.dir/main.cpp.o"
+  "Truck_Parking.pdb"
+  "Truck_Parking"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Truck_Parking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
