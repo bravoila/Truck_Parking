255889e746d9c7324635a7aca4183ffd2d143f7a
bravoila <mcl1001622@gmail.com>
2018-04-24 18:51:29 -0500

    Apr 24 2018

---
+++ tp_segment/cmake-build-debug/CMakeFiles/tp_segment.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tp_segment.dir/main.cpp.o"
+  "tp_segment.pdb"
+  "tp_segment"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tp_segment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
